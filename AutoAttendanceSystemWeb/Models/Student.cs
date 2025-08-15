7dd0a5b73c2b609ee1a1766d213e785be8ff52e9
Dwarkesh Ramani <rdwarkesh1300@gmail.com>
2025-08-15 08:45:42 +0530

    Add project files.

---
+++ AutoAttendanceSystemWeb/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoAttendanceSystemWeb.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Deaprtment { get; set; }
+        public int Semester { get; set; }
+        public char Division { get; set; }
+    }
+}
